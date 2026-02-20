b899374022f2a79d3042dead1c0a33c03c08ee17
Wolfyxon <wolfyxon@gmail.com>
2026-02-20 17:14:37 +0100

    Fixed links

--- scripts/allpage_script.js
+++ scripts/allpage_script.js
@@ -3,11 +3,11 @@ let last_updated = "23.12.2025"
 function AddNav(){
     document.querySelector("nav").innerHTML = `
                 <img src="../images/icons/KaroleusLogo.webp" alt="Karoleus logo">
-                <a id="nav_home" onmouseenter="NavButtonChange('nav_home', 1)"  onmouseleave="NavButtonChange('nav_home', 0)" href="../index.html"> HOME </a>
-                <a id="nav_about" onmouseenter="NavButtonChange('nav_about', 1)" onmouseleave="NavButtonChange('nav_about', 0)" href="about.html"> ABOUT </a>
-                <a id="nav_projects" onmouseenter="NavButtonChange('nav_projects', 1)" onmouseleave="NavButtonChange('nav_projects', 0)" href="projects.html"> PROJECTS </a>
-                <a id="nav_music" onmouseenter="NavButtonChange('nav_music', 1)" onmouseleave="NavButtonChange('nav_music', 0)" href="music.html"> MUSIC </a>
-                <a id="nav_games" onmouseenter="NavButtonChange('nav_games', 1)" onmouseleave="NavButtonChange('nav_games', 0)" class="LastNav" href="games.html"> WEB GAMES </a>
+                <a id="nav_home" onmouseenter="NavButtonChange('nav_home', 1)"  onmouseleave="NavButtonChange('nav_home', 0)" href="/index.html"> HOME </a>
+                <a id="nav_about" onmouseenter="NavButtonChange('nav_about', 1)" onmouseleave="NavButtonChange('nav_about', 0)" href="/subpages/about.html"> ABOUT </a>
+                <a id="nav_projects" onmouseenter="NavButtonChange('nav_projects', 1)" onmouseleave="NavButtonChange('nav_projects', 0)" href="/subpages/projects.html"> PROJECTS </a>
+                <a id="nav_music" onmouseenter="NavButtonChange('nav_music', 1)" onmouseleave="NavButtonChange('nav_music', 0)" href="/subpages/music.html"> MUSIC </a>
+                <a id="nav_games" onmouseenter="NavButtonChange('nav_games', 1)" onmouseleave="NavButtonChange('nav_games', 0)" class="LastNav" href="/subpages/games.html"> WEB GAMES </a>
                 <img src="../images/other/PLAYMYGAMES.jpg" id="PLAYMYGAMES" alt="Sonic.exe">
                 <p class="lastupdated_label">Last updated at ${last_updated}</p>
     `
